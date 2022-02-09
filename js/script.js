75e25b564b7a33494cede30757fa4d1215307ce2
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-02-08 21:41:27 -0300

    Colocado as bolhas, o peixe inimigo e um problema que tem resolver.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
 
-
 let score = 0;
 let gameFrame = 0;
-ctx.font = '50px Georgia';
+ctx.font = '40px Georgia';
+let gameSpeed = 1;
+let gameOver = false;
 
 //mouse interactivity
 let canvasPosition = canvas.getBoundingClientRect();
@@ -20,8 +21,6 @@ canvas.addEventListener('mousedown', function (event) {
     mouse.click = true;
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
-    //console.log(mouse.x, mouse.y);
-
 });
 canvas.addEventListener('mouseup', function () {
     mouse.click = false;
@@ -65,12 +64,12 @@ class Player {
             ctx.lineTo(mouse.x, mouse.y);
             ctx.stroke();
         }
-        ctx.fillStyle = 'red';
+        /*ctx.fillStyle = 'red';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
-        ctx.fillRect(this.x, this.y, this.radius, 10);
+        ctx.fillRect(this.x, this.y, this.radius, 10);*/
 
         ctx.save();
         ctx.translate(this.x, this.y);
@@ -87,16 +86,19 @@ class Player {
                 this.spriteWidth / 4, this.spriteHeight / 4);
         }
         ctx.restore();
-
     }
 }
 const player = new Player();
+
 //bubble
 const bubblesArray = [];
+const bubbleImage = new Image();
+bubbleImage.src = '../img/bubble_pop_one/bubble_pop_frame_01.png';
+
 class Bubble {
     constructor() {
         this.x = Math.random() * canvas.width;
-        this.y = canvas.height + 100 + Math.random() * canvas.height;
+        this.y = canvas.height + 100;
         this.radius = 50;
         this.speed = Math.random() * 5 + 1;
         this.distance;
@@ -110,17 +112,19 @@ class Bubble {
         this.distance = Math.sqrt(dx * dx + dy * dy);
     }
     draw() {
-        ctx.fillStyle = 'blue';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.stroke();
+        //ctx.fillStyle = 'blue';
+        //ctx.beginPath();
+        //ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        //ctx.fill();
+        //ctx.closePath();
+        //ctx.stroke();
+        ctx.drawImage(bubbleImage, 
+            this.x - 63, this.y - 63, 
+            this.radius * 2.5, this.radius * 2.5);
     }
 }
 const bubblePop1 = document.createElement('audio');
 bubblePop1.src = '../sound/pop.ogg';
-
 const bubblePop2 = document.createElement('audio');
 bubblePop2.src = '../sound/bubbles-single2.wav';
 
@@ -131,35 +135,126 @@ function handleBubbles() {
     for (let i = 0; i < bubblesArray.length; i++) {
         bubblesArray[i].update();
         bubblesArray[i].draw();
-    }
-    for (let i = 0; i < bubblesArray.length; i++) {
         if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
             bubblesArray.splice(i, 1);
-        }
-        if (bubblesArray[i]) {
-            if (bubblesArray[i].distance < bubblesArray[i].radius * player.radius) {
-                if (!bubblesArray[i].counted) {
-                    if (bubblesArray[i].sound == 'sound1') {
-                        bubblePop1.play();
-                    } else {
-                        bubblePop2.play();
-                    }
-                    score++;
-                    bubblesArray[i].counted = true;
+            i--;
+        } else if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+            if (!bubblesArray[i].counted) {
+                if (bubblesArray[i].sound == 'sound1') {
+                    bubblePop1.play();
+                } else {
+                    bubblePop2.play();
                 }
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i, 1);
+                i--;
             }
         }
     }
 }
+//Repeating Background
+const background = new Image();
+background.src = '../img/fundo/background1.png';
+/*
+const BG = {
+    x1: 0,
+    x2: canvas.width,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height,
+}
+function handleBackground() {
+    BG.x1 -= gameSpeed;
+    if (BG.x1 < -BG.width) BG.x1 = BG.width;
+    BG.x2 -= gameSpeed;
+    if (BG.x2 < -BG.width) BG.x2 = BG.width;
+    ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height);
+    ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
+};*/
+// Enemies
+const enemyImage = new Image();
+enemyImage.src = '../img/enemy/enemy2.png';
+
+class Enemy {
+    constructor() {
+        this.x = canvas.width + 200;
+        this.y = Math.random() * (canvas.height - 150) + 90;
+        this.radius = 60;
+        this.speed = Math.random() * 2 + 2;
+        this.frame = 0;
+        this.framex = 0;
+        this.frameY = 0;
+        this.spriteWidth = 218;
+        this.spriteHeight = 197;
+    }
+    draw() {
+        //ctx.fillStyle = 'gold';
+        //ctx.beginPath();
+        //ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        //ctx.fill();
+        //ctx.stroke();
+        /*ctx.drawImage(enemyImage, 
+            this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
+            this.spriteWidth, this.spriteHeights, this.x, this.y,
+            this.spriteWidth, this.spriteHeight); */
+            ctx.drawImage(enemyImage,
+                this.x - 63, this.y - 63, 
+                this.radius * 2.5, this.radius * 2.5);
+    }
+    update() {
+        this.x -= this.speed;
+        if (this.x < 0 - this.radius * 2) {
+            this.x = canvas.width + 200;
+            this.y = Math.random() * (canvas.height = 150) + 90;
+            this.speed = Math.random() * 2 + 2;
+        }
+        if (gameFrame % 5 == 0) {
+            this.frame++;
+            if (this.frame >= 12) this.frame = 0;
+            if (this.frame == 3 || this.frame == 7 || this.frame || 11) {
+                this.frameX = 0;
+            } else {
+                this.frameX++;
+            }
+            if (this.frame < 3) this.frameY = 0;
+            else if (this.frame < 7) this.frameY = 1;
+            else if (this.frame < 11) this.frameY = 2;
+            else this.frameY = 0;
+        }
+        // collision with player
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.radius + player.radius){
+            handleGameOver();
+        }
+    }
+}
+const enemy1 = new Enemy();
+function handleEnemies() {
+    enemy1.draw();
+    enemy1.update();
+}
+function handleGameOver(){
+    ctx.fillStyle = 'red';
+    ctx.fillText('GAME OVER, você perdeu! Seu score ' + score, 60, 250);
+    gameOver = true;
+}
 //animation loop
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //handleBackground();
     handleBubbles();
     player.update();
     player.draw();
+    handleEnemies();
     ctx.fillStyle = 'black';
-    ctx.fillText('Score: ' + score, 10, 50,);
+    ctx.fillText('Score: ' + score, 10, 50);
     gameFrame++;
-    requestAnimationFrame(animate);
+    if(!gameOver)requestAnimationFrame(animate);
 }
 animate();
+window.addEventListener('resize', function () {
+    canvasPosition = canvas.getBoundingClientRect();
+});
